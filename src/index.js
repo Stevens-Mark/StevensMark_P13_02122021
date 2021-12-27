858b7dfbee50bc692dada147a52d58923e817eb2
Mark Stevens <markspstevens@yahoo.co.uk>
2021-12-27 07:13:37 +0100

    change to configureStore - redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from "react-redux";
-import { store } from '../src/utils/store/store'
-import { createGlobalStyle } from "styled-components";
-import colors from './utils/style/colors';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from "react-redux"
+import { createGlobalStyle } from "styled-components"
+import colors from './utils/style/colors'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+// import store
+import store from '../src/utils/store/store'
+
 
 /**
  * CSS Global styles for the site using styled.components
@@ -64,4 +66,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/utils/store/store.js
+++ src/utils/store/store.js
@@ -1,26 +1,12 @@
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { tokenReducer } from '../../features/fetchToken'
 import { userReducer } from '../../features/fetchUpdateUser'
-/**
-* put all reducers together
-*/
- const rootReducer = combineReducers({
-  token: tokenReducer,
-  userStats: userReducer,
+
+// create the store & put all reducers together
+
+export default configureStore({
+    reducer: {
+      token: tokenReducer,
+      userStats: userReducer,
+    },
 })
-
-// create the store & connect devtools
-const reduxDevtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // This is the chrome Redux plugin
-export const store = createStore(rootReducer, reduxDevtools)
-
-
-
-
-// import { configureStore } from '@reduxjs/toolkit'
- 
-// export default configureStore({
-//     reducer: {
-//       token: tokenReducer,
-//       userStats: userReducer,
-//     },
-// })
